1ffc0e77a5bc6d34864db12bae391a4bddf225fb
Vladimir Melnic <win32hack@gmail.com>
2016-05-12 21:04:29 +0300

    Fix vagrant machine dns.

--- provision/bash/preinstall.sh
+++ provision/bash/preinstall.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-echo -e "\n[*] Run update command.\n"
+echo -e "\n[*] Fix name server.\n"
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 apt-get update --fix-missing;
 echo -e "\n[*] Install python-pip, python-dev, git, curl, wget, nano.\n"
