c7c490e7e61edc17b55a13e0e4f77c7270016a99
Vladimir Melnic <win32hack@gmail.com>
2016-06-03 13:06:11 +0300

    Updated preinstall.sh script.

--- provision/bash/preinstall.sh
+++ provision/bash/preinstall.sh
@@ -3,7 +3,7 @@ echo -e "\n[*] Fix name server.\n"
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 apt-get update --fix-missing;
 echo -e "\n[*] Install python-pip, python-dev, git, curl, wget, nano.\n"
-apt-get install -y python-pip python-dev git curl wget nano;
+apt-get install -y build-essential libssl-dev libffi-dev python-dev python-pip git curl wget nano;
 echo -e "\n[*] Install markupsafe.\n"
 pip install markupsafe;
 echo -e "\n[*] Install ansible.\n"
